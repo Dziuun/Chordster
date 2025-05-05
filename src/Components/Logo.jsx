e267a40b78d98bfc8502a7e91e6543c44cc45487
Dziuun <kamil.dziobekk@gmail.com>
2025-05-05 15:59:46 +0200

    Created first view

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,5 @@
+function Logo() {
+  return <h1>Chordster</h1>;
+}
+
+export default Logo;
---
+++ src/Views/ChordFinderView.jsx
@@ -0,0 +1,17 @@
+import Logo from "../Components/Logo";
+
+function ChordFinderView() {
+  return (
+    <div>
+      <Logo />
+      <h2>CHORD NAME</h2>
+      <div>
+        <div>Guitar</div>
+      </div>
+      <button>Play</button>
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default ChordFinderView;
