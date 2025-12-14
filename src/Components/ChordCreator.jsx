44b557294267367b47554d4a16d6df3db82a137e
Dziuun <kamil.dziobekk@gmail.com>
2025-12-14 01:28:37 +0100

    trying to figure put the guitar component

--- src/Components/ChordCreator.jsx
+++ src/Components/ChordCreator.jsx
@@ -1,249 +1,99 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import styles from "./ChordCreator.module.css";
 
 const initialState = {
   strings: [
     {
       string: "E",
-      frets: Array.from({ length: 5 }, (_, i) => ({
-        id: i + 1,
+      frets: Array.from({ length: 24 }, (_, i) => ({
         pressed: false,
+        muted: false,
+      })),
+    },
+    {
+      string: "A",
+      frets: Array.from({ length: 24 }, (_, i) => ({
+        pressed: false,
+        muted: false,
+      })),
+    },
+    {
+      string: "D",
+      frets: Array.from({ length: 24 }, (_, i) => ({
+        pressed: false,
+        muted: false,
+      })),
+    },
+    {
+      string: "G",
+      frets: Array.from({ length: 24 }, (_, i) => ({
+        pressed: false,
+        muted: false,
+      })),
+    },
+    {
+      string: "B",
+      frets: Array.from({ length: 24 }, (_, i) => ({
+        pressed: false,
+        muted: false,
+      })),
+    },
+    {
+      string: "e",
+      frets: Array.from({ length: 24 }, (_, i) => ({
+        pressed: false,
+        muted: false,
       })),
     },
   ],
+  scroll: 0,
+  pressed: [0, 0, 0, 0, 0, 0],
 };
 
-function ChordCreator() {
-  const [curFingers, setCurFingers] = useState([]);
-  const [scrollValue, setScrollValue] = useState(0);
+function reducer(state, action) {
+  switch (action.type) {
+    case "fret/pressed": {
+      const id = action.payload;
+      const stringName = id[0];
+      const fretPressed = Number(id.slice(1));
+      const stringPressed = state.strings.filter(
+        (string) => string.string === stringName
+      );
+      const location = stringPressed[0].frets[fretPressed];
 
-  const [stringE, setStringE] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  const [stringA, setStringA] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  const [stringD, setStringD] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  const [stringG, setStringG] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  const [stringB, setStringB] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  const [stringLowE, setStringLowE] = useState(
-    Array.from({ length: 5 }, (_, i) => ({
-      id: i + 1,
-      pressed: false,
-    }))
-  );
-
-  function handleClick(e) {
-    const id = +e.target.id.slice(1, 2);
-
-    const baseNote = e.target.id.slice(0, 1);
-
-    console.log(id, baseNote);
-
-    switch (baseNote) {
-      case "E":
-        setStringE((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-
-        setCurFingers((prev) => [...prev, (prev[0] = id)]);
-        console.log(curFingers);
-        break;
-
-      case "A":
-        setStringA((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-        break;
-
-      case "D":
-        setStringD((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-        break;
-
-      case "G":
-        setStringG((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-        break;
-
-      case "B":
-        setStringB((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-        break;
-
-      case "e":
-        if (stringLowE.filter((f) => f.pressed).length) {
-          setStringLowE((prev) => prev.map());
-          break;
-        }
-
-        setStringLowE((prev) =>
-          prev.map((fret) =>
-            fret.id === id ? { ...fret, pressed: !fret.pressed } : fret
-          )
-        );
-        break;
+      console.log(stringPressed[0].frets[fretPressed]);
+      return { ...state };
     }
-
-    console.log("note sound");
   }
+}
+
+function ChordCreator() {
+  const [{ strings, scroll }, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className={styles.chordCreatorContainer}>
       <div className={styles.stockMap}>
-        <div className={styles.string}>
-          {stringE.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`E${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`E${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.string}>
-          {stringA.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`A${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`A${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.string}>
-          {stringD.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`D${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`D${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.string}>
-          {stringG.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`G${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`G${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.string}>
-          {stringB.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`B${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`B${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.string}>
-          {stringLowE.map((fret) => {
-            return (
-              <div
-                className={styles.fret}
-                id={`e${fret.id}`}
-                onClick={(e) => {
-                  handleClick(e);
-                }}
-              >
-                {fret.pressed ? (
-                  <span className={styles.finger} id={`e${fret.id}`}></span>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
+        {strings.map((string) => (
+          <div className={styles.string}>
+            {Array.from({ length: 5 }).map((_, i) => {
+              return (
+                <div
+                  className={styles.fret}
+                  id={`${string.string}${i + 1 + scroll}`}
+                  onClick={(e) =>
+                    dispatch({ type: "fret/pressed", payload: e.target.id })
+                  }
+                >
+                  {/* {fret.pressed ? (
+                    <span className={styles.finger} id={`E${fret.id}`}></span>
+                  ) : (
+                    ""
+                  )} */}
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
